1984de2bc7a02cfe9afcde79a29d1801e3d3e5ad
Harsh3305 <harshverma3305@gmail.com>
2022-08-22 12:09:02 +0530

    First Commit

---
+++ src/main/kotlin/com/courseinsides/courseinsidesbackend/CourseInsidesBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.courseinsides.courseinsidesbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CourseInsidesBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<CourseInsidesBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/courseinsides/courseinsidesbackend/CourseInsidesBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.courseinsides.courseinsidesbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseInsidesBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
